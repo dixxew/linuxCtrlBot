1b451916dc66ba9f328f1e9da4ee718c6f24568c
dixxew <ayrat.volodin2@gmail.com>
2024-12-03 21:06:29 +0300

    Add showing commands

--- linuxCtrlBot/Program.cs
+++ linuxCtrlBot/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 class Program
@@ -10,10 +11,12 @@ class Program
 
     static async Task Main(string[] args)
     {
-        string botToken = "7879607825:AAGGXGU5RHm7U5QKSfHxqx-UuJMILZF_GuQ";
+        string botToken = "YOUR_BOT_TOKEN";
         _botClient = new TelegramBotClient(botToken);
 
         Auth.LoadUserRoles();
+
+        await SetBotCommands(_botClient);
 
         var me = await _botClient.GetMeAsync();
         Console.WriteLine($"Bot {me.Username} started");
@@ -29,4 +32,29 @@ class Program
         Console.ReadLine();
         cts.Cancel();
     }
+
+    private static async Task SetBotCommands(TelegramBotClient botClient)
+    {
+        // Команды для роли "user"
+        var userCommands = new List<BotCommand>
+    {
+        new BotCommand { Command = "/ip_a", Description = "Показать IP адреса" },
+        new BotCommand { Command = "/speedtest", Description = "Тест скорости интернета" },
+        new BotCommand { Command = "/ls", Description = "Показать файлы в /root/" },
+        new BotCommand { Command = "/createVpnConfig", Description = "Создать VPN конфиг" }
+    };
+
+        // Команды для роли "admin"
+        var adminCommands = new List<BotCommand>(userCommands)
+    {
+        new BotCommand { Command = "/adduser", Description = "Добавить нового пользователя" }
+    };
+
+        // Устанавливаем команды для всех пользователей
+        await botClient.SetMyCommandsAsync(userCommands, new BotCommandScopeAllPrivateChats());
+
+        // Устанавливаем команды для администраторов
+        await botClient.SetMyCommandsAsync(adminCommands, new BotCommandScopeAllPrivateChats());
+    }
+
 }
